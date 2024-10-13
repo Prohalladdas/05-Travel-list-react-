e9cf66a94a135caef43c212dd11ffc144e8d799e
Prohalladdas <prolladdas@gmail.com>
2024-10-13 15:00:00 +0530

    Building a Form and Handling Submissions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,16 +21,34 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
+  function HandleSubmit(e) {
+    e.preventDefault()
+    console.log(e);
+
+  }
+
+  return <form className="add-form" onSubmit={HandleSubmit}>
     <h3 >What do you need for your 😊 trip?</h3>
-  </div>
+    <select>
+      {Array.from({ length: 20 }, (_, i) => i + 1).map(
+        (num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+    </select>
+    <input type="text" placeholder="Item..." />
+    <button>Add</button>
+  </form>
 }
 
 function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (<Item item={item} />))}
+        {initialItems.map((item) =>
+          (<Item item={item} key={item.id} />)
+        )}
       </ul>
     </div>)
 }
