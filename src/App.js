58473a2b5c162be6d66c30ec908e992219c27631
Prohalladdas <prolladdas@gmail.com>
2024-10-21 20:23:36 +0530

    Deleting an Item More Child-to-Parent Communication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 // import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 9, packed: false },
-];
-
 export default function App() {
   const [items, setItem] = useState([])
 
